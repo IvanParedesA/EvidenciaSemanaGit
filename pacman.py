386d86923b987d85fc9a4d65935b80094a2af6f4
IvanParedesA <a01705083@tec.mx>
2024-03-22 13:05:06 -0600

    modify and add new comments

--- pacman.py
+++ pacman.py
@@ -3,10 +3,14 @@ from turtle import Turtle, bgcolor, clear, up, goto, dot, update
 from turtle import ontimer, setup, hideturtle, tracer, listen, onkey, done
 from freegames import floor, vector
 
-
+# Estado del juego
 state = {'score': 0}
+
+# Inicialización de la interfaz de Turtle
 path = Turtle(visible=False)
 writer = Turtle(visible=False)
+
+# Posiciones y velocidades iniciales de los elementos del juego
 aim = vector(5, 0)
 pacman = vector(-40, -80)
 ghosts = [
@@ -15,6 +19,8 @@ ghosts = [
     [vector(100, 160), vector(0, -5)],
     [vector(100, -160), vector(-5, 0)],
 ]
+
+# Diseño del mapa
 tiles = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
@@ -39,8 +45,8 @@ tiles = [
 ]
 
 
+# Función para dibujar un cuadrado
 def square(x, y):
-    "Draw square using path at (x, y)."
     path.up()
     path.goto(x, y)
     path.down()
@@ -53,16 +59,16 @@ def square(x, y):
     path.end_fill()
 
 
+# Función para obtener el índice de un punto en las "tiles" (casillas)
 def offset(point):
-    "Return offset of point in tiles."
     x = (floor(point.x, 20) + 200) / 20
     y = (180 - floor(point.y, 20)) / 20
     index = int(x + y * 20)
     return index
 
 
+# Función para verificar si un punto es válido en las "tiles" (casillas)
 def valid(point):
-    "Return True if point is valid in tiles."
     index = offset(point)
 
     if tiles[index] == 0:
@@ -76,8 +82,8 @@ def valid(point):
     return point.x % 20 == 0 or point.y % 20 == 0
 
 
+# Función para dibujar el mapa
 def world():
-    "Draw world using path."
     bgcolor('black')
     path.color('blue')
 
@@ -95,8 +101,8 @@ def world():
                 path.dot(2, 'white')
 
 
+# Función para mover los elementos del juego
 def move():
-    "Move pacman and all ghosts."
     writer.undo()
     writer.write(state['score'])
 
@@ -145,13 +151,14 @@ def move():
     ontimer(move, 100)
 
 
+# Función para cambiar la dirección de pacman
 def change(x, y):
-    "Change pacman aim if valid."
     if valid(pacman + vector(x, y)):
         aim.x = x
         aim.y = y
 
 
+# Configuración inicial de la pantalla
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
